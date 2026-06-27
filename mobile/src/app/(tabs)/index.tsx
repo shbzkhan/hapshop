b9c35168e2936805bb47c81d2a1a6903d649bcd0
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-06-27 10:48:38 +0100

    setup tailwind css and designed header of home screen

--- mobile/src/app/(tabs)/index.tsx
+++ mobile/src/app/(tabs)/index.tsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import downArrowIcon from "@/assets/images/down-arrow.png"
+import buyIcon from "@/assets/images/Buy.png"
+import notificationIcon from "@/assets/images/Notification.png"
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <SafeAreaView className='flex-1 bg-white'>
+    <View className='px-4 mt-2'>
+    <View className='flex-row justify-between items-center'>
+      <View className=''>
+        <Text className='text-gray text-sm'>Delivery address</Text>
+        <TouchableOpacity className='flex-row items-center justify-center -mt-1 gap-1'>
+          <Text className='text-[0.920rem] text-black font-medium'>Navi Mumbai, Maharastra</Text>
+          <View className='items-center justify-center'>
+            <Image source={downArrowIcon}
+            // className='w-4 h-4'
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View className='flex-row justify-center items-center gap-4'>
+        <TouchableOpacity>
+          <Image source={buyIcon}
+            // className='w-6 h-4'
+            resizeMode='contain'
+            />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={notificationIcon}
+            // className='w-4 h-4'
+            resizeMode='contain'
+            />
+        </TouchableOpacity>
+      </View>
     </View>
+    <View>
+      <Text>Search</Text>
+    </View>
+    </View>
+    </SafeAreaView>
   )
 }
 
---
+++ mobile/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+          black: "#393F42",
+          gray: "#C8C8CB",
+          border: "#F0F2F1",
+          primary: "#67C4A7"
+      }
+    },
+  },
+  plugins: [],
+}
