f13025cfe7f2f59442155d98fc469489a8efd241
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-07-10 19:05:35 +0530

    adde expected date

--- mobile/src/modules/product/components/DeliveryDetails.tsx
+++ mobile/src/modules/product/components/DeliveryDetails.tsx
@@ -4,17 +4,29 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 const DeliveryDetails = () => {
+
+    function getExpectedDeliveryDate(bufferDays:number = 7) {
+    const today:Date = new Date();
+    
+    today.setDate(today.getDate() + bufferDays);
+    
+    const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', weekday:'short' };
+    return today.toLocaleDateString('en-IN', options); 
+}
+
+console.log("Expected Delivery:", getExpectedDeliveryDate());
+
   return (
     <View className='gap-3'>
       <Text className='text-xl font-semibold text-black'>Delivery Details</Text>
       <View className='rounded-xl overflow-hidden gap-1 mb-7'>
         <Pressable className='flex-row items-center gap-2 bg-primary/20 px-4 py-2.5'>
             <Ionicons name="location-outline" size={16} color="black" />
-            <Text numberOfLines={1} className='text-lg'>mankhurd, Navi Mumbai, India, 400043</Text>
+            <Text numberOfLines={1} className='text-lg'>Mankhurd, Navi Mumbai, India, 400043</Text>
         </Pressable>
         <Pressable className='flex-row items-center gap-2 bg-border px-4 py-2.5 '>
             <MaterialCommunityIcons name="truck-delivery-outline" size={16} color="black" />
-            <Text numberOfLines={1} className='text-lg font-semibold'>Delivery by 23 Jul, Thu</Text>
+            <Text numberOfLines={1} className='text-lg font-semibold'>Delivery by {getExpectedDeliveryDate()}</Text>
         </Pressable>
         <Pressable className='flex-row  gap-2 bg-border px-4 py-2.5'>
             <View className='py-1'>
@@ -24,7 +36,7 @@ const DeliveryDetails = () => {
             <Text className='text-lg text-black'>FullFields by Agent travel</Text>
             <Text className='text-sm text-black'>4.7 * . 6 years with HapShop</Text>
             <TouchableOpacity>
-                <Text className='text-primary'>See other sellers</Text>
+                <Text className='text-primary text-sm'>See other sellers</Text>
             </TouchableOpacity>
 
             </View>
