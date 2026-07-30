b0f93575131f1b977fa900a34e54fb57105f49fb
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2026-07-30 22:52:03 +0530

    fixed connection error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,28 @@
 import dotenv from "dotenv"
+import { connectDB } from "./db/index.ts"
+import { app } from "./app.ts"
+import dns from "dns"
+
+//change dns
+dns.setServers(["1.1.1.1", "8.8.8.8"])
+
 
 dotenv.config({
     path:"./env"
 })
+
+
+
+
+connectDB()
+        .then(()=>{
+            app.on("error", error=>{
+                    console.log("MONGODB CONNECTION ERROR ", error)
+            })
+           app.listen(process.env.PORT || 5000, () => {
+            console.log("Server run on port: ", process.env.PORT || 5000);
+            })
+        })
+        .catch(error => {
+            console.log("MONGODB CONNECTION FAILED!!!", error);
+        })
